08b694199fdb004c88de74494667e787f4f9f35a
Vivek Ranchhod Patel <vivek.patel@eedeecreations.com>
2024-04-11 19:31:26 +0530

    Initial commit

--- src/app/components/Slider/page.jsx
+++ src/app/components/Slider/page.jsx
@@ -1,115 +1,115 @@
-"use client"
-import React, { useState, useEffect } from "react";
+// "use client"
+// import React, { useState, useEffect } from "react";
 
-import "./page.css";
+// import "./page.css";
 
-function CustomCarousel({ children }) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [slideDone, setSlideDone] = useState(true);
-  const [timeID, setTimeID] = useState(null);
-const [childrenState,setChildrenState]=useState([]);
-setChildrenState(children);
-console.log(childrenState)
-  useEffect(() => {
-    if (slideDone) {
-      setSlideDone(false);
-      setTimeID(
-        setTimeout(() => {
-          slideNext();
-          setSlideDone(true);
-        }, 5000)
-      );
-    }
-  }, [slideDone]);
+// function CustomCarousel({ children }) {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   const [slideDone, setSlideDone] = useState(true);
+//   const [timeID, setTimeID] = useState(null);
+// const [childrenState,setChildrenState]=useState([]);
+// setChildrenState(children);
+// console.log(childrenState)
+//   useEffect(() => {
+//     if (slideDone) {
+//       setSlideDone(false);
+//       setTimeID(
+//         setTimeout(() => {
+//           slideNext();
+//           setSlideDone(true);
+//         }, 5000)
+//       );
+//     }
+//   }, [slideDone]);
 
-  const slideNext = () => {
-    setActiveIndex((val) => {
-      if (val >= children.length - 1) {
-        return 0;
-      } else {
-        return val + 1;
-      }
-    });
-  };
+//   const slideNext = () => {
+//     setActiveIndex((val) => {
+//       if (val >= children.length - 1) {
+//         return 0;
+//       } else {
+//         return val + 1;
+//       }
+//     });
+//   };
 
-  const slidePrev = () => {
-    setActiveIndex((val) => {
-      if (val <= 0) {
-        return children.length - 1;
-      } else {
-        return val - 1;
-      }
-    });
-  };
+//   const slidePrev = () => {
+//     setActiveIndex((val) => {
+//       if (val <= 0) {
+//         return children.length - 1;
+//       } else {
+//         return val - 1;
+//       }
+//     });
+//   };
 
-  const AutoPlayStop = () => {
-    if (timeID > 0) {
-      clearTimeout(timeID);
-      setSlideDone(false);
-    }
-  };
+//   const AutoPlayStop = () => {
+//     if (timeID > 0) {
+//       clearTimeout(timeID);
+//       setSlideDone(false);
+//     }
+//   };
 
-  const AutoPlayStart = () => {
-    if (!slideDone) {
-      setSlideDone(true);
-    }
-  };
+//   const AutoPlayStart = () => {
+//     if (!slideDone) {
+//       setSlideDone(true);
+//     }
+//   };
 
-  return (
-    <div
-      className="container__slider"
-      onMouseEnter={AutoPlayStop}
-      onMouseLeave={AutoPlayStart}
-    >
-      {childrenState.map((item,index)=>{
-        return(
-            <div
-            className={"slider__item slider__item-active-" + (activeIndex + 1)}
-            key={index}
-          >
-            {item}
-          </div>
-        )
-      })}
-      <div className="container__slider__links">
-        {childrenState.map((item, index) => {
-          return (
-            <button
-              key={index}
-              className={
-                activeIndex === index
-                  ? "container__slider__links-small container__slider__links-small-active"
-                  : "container__slider__links-small"
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                setActiveIndex(index);
-              }}
-            ></button>
-          );
-        })}
-      </div>
+//   return (
+//     <div
+//       className="container__slider"
+//       onMouseEnter={AutoPlayStop}
+//       onMouseLeave={AutoPlayStart}
+//     >
+//       {childrenState.map((item,index)=>{
+//         return(
+//             <div
+//             className={"slider__item slider__item-active-" + (activeIndex + 1)}
+//             key={index}
+//           >
+//             {item}
+//           </div>
+//         )
+//       })}
+//       <div className="container__slider__links">
+//         {childrenState.map((item, index) => {
+//           return (
+//             <button
+//               key={index}
+//               className={
+//                 activeIndex === index
+//                   ? "container__slider__links-small container__slider__links-small-active"
+//                   : "container__slider__links-small"
+//               }
+//               onClick={(e) => {
+//                 e.preventDefault();
+//                 setActiveIndex(index);
+//               }}
+//             ></button>
+//           );
+//         })}
+//       </div>
 
-      <button
-        className="slider__btn-next"
-        onClick={(e) => {
-          e.preventDefault();
-          slideNext();
-        }}
-      >
-        {">"}
-      </button>
-      <button
-        className="slider__btn-prev"
-        onClick={(e) => {
-          e.preventDefault();
-          slidePrev();
-        }}
-      >
-        {"<"}
-      </button>
-    </div>
-  );
-}
+//       <button
+//         className="slider__btn-next"
+//         onClick={(e) => {
+//           e.preventDefault();
+//           slideNext();
+//         }}
+//       >
+//         {">"}
+//       </button>
+//       <button
+//         className="slider__btn-prev"
+//         onClick={(e) => {
+//           e.preventDefault();
+//           slidePrev();
+//         }}
+//       >
+//         {"<"}
+//       </button>
+//     </div>
+//   );
+// }
 
-export default CustomCarousel;
+// export default CustomCarousel;
