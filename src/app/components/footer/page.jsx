6426340e3d3466b81d07448be0da6c5ac464b2a9
Vivek Ranchhod Patel <vivek.patel@eedeecreations.com>
2024-08-31 16:11:49 +0530

    Added footer link to navigate to admin login

--- src/app/components/footer/page.jsx
+++ src/app/components/footer/page.jsx
@@ -50,6 +50,7 @@ export default function page() {
                 <li className="hover:underline">Admissions</li>
                 <li className="hover:underline">IQAC</li>
                 <li className="hover:underline">Community</li>
+                <li className="hover:underline"><Link to={"/Admin/AdminLogin/"}>Admin Login</Link></li>
               </ul>
             </div>
           </div>
