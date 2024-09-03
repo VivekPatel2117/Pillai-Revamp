6324205244ec389a34c75ff7ba09d9389c5c8806
Vivek Ranchhod Patel <vivek.patel@eedeecreations.com>
2024-09-03 10:01:41 +0530

    added some logs

--- src/backend/lib/connect.js
+++ src/backend/lib/connect.js
@@ -9,9 +9,22 @@ export const connectToDatabase = async () => {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGODB_URI)
-    console.log(db,"Connection")
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    
     isConnected = db.connections[0].readyState;
+
+    // Log the database name
+    const dbName = db.connections[0].name;
+    console.log(`=> Connected to database: ${dbName}`);
+
+    // List available collections
+    const collections = await db.connection.db.listCollections().toArray();
+    const collectionNames = collections.map(col => col.name);
+    console.log('=> Available collections:', collectionNames);
+
     console.log('=> New database connection established');
   } catch (error) {
     console.error('=> Database connection failed', error);
